8001a59a0f2d10804b5d7b9b84cc00cb2c2e9436
Martin Löffler <me@fatalerrorcoded.eu>
2022-01-22 22:14:30 +0100

    feat(ftl_protocol): reply to client with error

--- crates/ftl/src/protocol/error.rs
+++ crates/ftl/src/protocol/error.rs
@@ -1,12 +1,18 @@
+use std::string::ToString;
+
 #[derive(Debug)]
 pub enum FtlError {
-    NoLabel,
     IoError,
+    AllocateError,
     RingError,
     DecodeError,
     MissingPart,
-    ExternalError,
-    Unauthenticated,
+
+    InvalidStreamKey, // applies to channel id as well
+    ChannelNotAuthorized,
+    ChannelInUse,
+    UnsupportedRegion,
+    GameBlocked,
     
     InvalidProtocolVersion,
     UnsupportedProtocolVersion,
@@ -21,6 +27,30 @@ impl FtlError {
         match self {
             FtlError::Disconnect => false,
             _ => true
+        }
+    }
+}
+
+impl ToString for FtlError {
+    fn to_string(&self) -> String {
+        match self {
+            FtlError::IoError => "500 Internal Server Error\n".to_string(),
+            FtlError::AllocateError => "500 Internal Server Error\n".to_string(),
+            FtlError::RingError => "400 HMAC Decode Error\n".to_string(),
+            FtlError::DecodeError => "400 HMAC Decode Error\n".to_string(),
+            FtlError::MissingPart => "400 Bad Request\n".to_string(),
+
+            FtlError::InvalidStreamKey => "405 Invalid stream key\n".to_string(),
+            FtlError::ChannelNotAuthorized => "401 Channel not authorized to stream\n".to_string(),
+            FtlError::ChannelInUse => "406 Channel actively streaming\n".to_string(),
+            FtlError::UnsupportedRegion => "407 Streaming from your region is not authorized\n".to_string(),
+            FtlError::GameBlocked => "409 Channel is not allowed to stream set game\n".to_string(),
+
+            FtlError::InvalidProtocolVersion => "400 Invalid Protocol Version\n".to_string(),
+            FtlError::UnsupportedProtocolVersion => "402 Outdated FTL SDK version\n".to_string(),
+            FtlError::MissingCodecInformation => "400 Missing Codec Information\n".to_string(),
+            FtlError::UnimplementedCommand => "901 Invalid Command\n".to_string(),
+            _ => "".to_string(),
         }
     }
 }
