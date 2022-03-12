62b576c875cfefc5fb588744bcf3505e3dce1602
Paul Makles <paulmakles@gmail.com>
2022-03-12 12:43:07 +0000

    chore: bump broadcast version

--- crates/broadcast/src/lib.rs
+++ crates/broadcast/src/lib.rs
@@ -9,10 +9,6 @@ extern crate lazy_static;
 #[cfg_attr(docsrs, doc(cfg(feature = "rtc")))]
 pub mod rtc;
 
-#[cfg(feature = "ingest")]
-#[cfg_attr(docsrs, doc(cfg(feature = "ingest")))]
-pub mod ingest;
-
 #[cfg(feature = "signaling")]
 #[cfg_attr(docsrs, doc(cfg(feature = "signaling")))]
 pub mod signaling;
